a09b6e88cb189654390be51f9e599a822abbbea8
qihang <854883819@qq.com>
2019-05-20 10:43:30 +0800

    ssh

--- HooCoreText/HooCoreTextView/HooTextAttachment.h
+++ HooCoreText/HooCoreTextView/HooTextAttachment.h
@@ -15,7 +15,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface HooTextAttachment : NSObject
 
 @property (nonatomic,strong) id content;
-@property (nonatomic) UIEdgeInsets contentInsets;
+@property (nonatomic) UIEdgeInsets contentInsets; //附件在CTRun的相对位置
 
 //@property (nonatomic)
 
